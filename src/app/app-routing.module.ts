79b63e6d8906ac428cb8a0bbacfe0913eee05a17
Tashi Wangdi <twangdi63@gmail.com>
2021-04-13 21:08:56 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,14 @@ import { NotificationComponent } from './notification/notification.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 const routes: Routes = [
-  {path:'login', component:LoginComponent},
-  {path:'nav-bar', component:NavBarComponent},
-  {path: 'create-new', component: CreateNewComponent},
-  {path: 'forgot-pwd', component: ForgotPwdComponent},
-  {path:'add-new', component: AddNewComponent},
+  {path:'', component:LoginComponent},
+  {path:'navbar', component:NavBarComponent},
+  {path: 'createnew', component: CreateNewComponent},
+  {path: 'forgotpwd', component: ForgotPwdComponent},
+  {path:'addnew', component: AddNewComponent},
   {path:'notification', component: NotificationComponent},
-  {path:'about-us', component: AboutUsComponent},
-  {path: 'contact-us', component: ContactUsComponent}
+  {path:'aboutus', component: AboutUsComponent},
+  {path: 'contactus', component: ContactUsComponent}
 ];
 
 @NgModule({
